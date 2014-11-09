000481237ab8c025634968bb64ce6d67abd72afe
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-11-09 21:39:52 +0400

    last

---
+++ generate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+subprocess.Popen('pyside-rcc res/resources.qrc -o resources_rc.py')
+subprocess.Popen('pyside-uic res/main.ui -o main.py')
--- mass_graph.py
+++ mass_graph.py
@@ -3,8 +3,6 @@
 
 import numpy as n
 import matplotlib.pyplot as p
-import subprocess as s
-import os
 import sys
 from PySide.QtGui import *
 from main import Ui_Form
@@ -15,9 +13,6 @@ class MainWidget(QWidget, Ui_Form):
         self.setupUi(self)
 
 def main():
-    if not os.path.exists('resources_rc.py'): s.Popen('pyside-rcc res/resources.qrc -o resources_rc.py')
-    if not os.path.exists('main.py'):         s.Popen('pyside-uic res/main.ui -o main.py')
-    
     app = QApplication(sys.argv)
     
     m = MainWidget()
